ed4426d2599965f1e3afce60c9b6bf80dff5c09d
vstecnic <vstecnic@gmail.com>
2024-04-14 01:44:18 -0300

    paste proyecto anterior

---
+++ js/carrito.js
@@ -0,0 +1,31 @@
+
+
+function mensaje() {
+    var resultado = window.confirm('Eliminar producto?');
+if (resultado === true) {
+    window.alert('Okay, lo eliminamos.');
+} else { 
+    window.alert('Volvamos a la compra!');
+}
+}
+
+
+function mensaje2() {
+    var resultado = window.confirm('¿Vaciamos el carrito?');
+    if (resultado === true) {
+        window.alert('Okay, lo vaciamos!');
+    } else { 
+        window.alert('Volvamos a la compra!');
+    }
+}
+
+
+function mensaje3() {
+        window.alert('Nada que ver aquí, circulen...');
+
+}
+
+
+
+
+
