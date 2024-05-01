0c6fed9a81bb19cabb0533075ed3bafe3ff40a50
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-01 09:55:16 -0300

    estructura componentes y routes

---
+++ FrontEnd/src/app/pages/auth/iniciar-sesion/iniciar-sesion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciar-sesion',
+  standalone: true,
+  imports: [],
+  templateUrl: './iniciar-sesion.component.html',
+  styleUrl: './iniciar-sesion.component.css'
+})
+export class IniciarSesionComponent {
+
+}
---
+++ FrontEnd/src/app/pages/pagina404/pagina404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina404',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina404.component.html',
+  styleUrl: './pagina404.component.css'
+})
+export class Pagina404Component {
+
+}
