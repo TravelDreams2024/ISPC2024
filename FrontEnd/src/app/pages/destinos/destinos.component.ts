5885c33ddd6ec25496df22cf69599dcac7fee735
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-30 21:09:27 -0300

    components

---
+++ FrontEnd/src/app/pages/destinos/destinos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destinos',
+  standalone: true,
+  imports: [],
+  templateUrl: './destinos.component.html',
+  styleUrl: './destinos.component.css'
+})
+export class DestinosComponent {
+
+}
