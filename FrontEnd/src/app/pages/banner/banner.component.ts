999c5f56ce2a94b708c319631d5f1d319a3a5b97
Claudio Fernandez <cc.rr.ff.2014@gmail.com>
2024-05-10 17:45:25 -0300

    Componente banner agrego al Home

---
+++ FrontEnd/src/app/pages/banner/banner.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './banner.component.html', 
+  styleUrl: './banner.component.css'
+})
+export class BannerComponent {
+  titulo1: string = "Hacemos Realidad tus Sueños";
+  subtitulo1: string = "¡Vive con nosotros la mejor Experiencia en Giras de Estudio que siempre quisiste!";
+  titulo2: string = "¡Viajes que inspiran, destinos que cautivan!";
+  subtitulo2: string = "Descubre nuevos horizontes con nosotros:";
+  titulo3: string = "Explora el mundo con confianza:";
+  subtitulo3: string = "¡Donde cada destino es una aventura, cada experiencia es inolvidable!";
+}
--- FrontEnd/src/app/pages/home/home.component.ts
+++ FrontEnd/src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { DestinosComponent } from '../destinos/destinos.component';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from '../banner/banner.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, DestinosComponent],
+  imports: [RouterOutlet, CommonModule, BannerComponent, DestinosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  titulo: string = "Nuestros Destinos";
 }
