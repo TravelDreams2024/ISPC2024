9c344e81014e234cfe853b2853c434f3830ff075
NatG <101484974+Natalia24v@users.noreply.github.com>
2024-05-13 23:32:19 -0300

    subo login

--- FrontEnd/src/app/pages/auth/iniciar-sesion/iniciar-sesion.component.ts
+++ FrontEnd/src/app/pages/auth/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
 export class IniciarSesionComponent {
+
+  formGroup = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      rememberMe: new FormControl(false, Validators.requiredTrue)
+
+  });
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+}
+clickRegister(): void{
+  // console.log(this.formGroup.get('name')?.value as string);
+  const name = this.formGroup.controls.email.value
+  console.log(name)
+}
+onEnviar(event: Event) {
+  console.log(this.formGroup.value);
+  event.preventDefault();
+  if (this.formGroup.valid) {
+    alert("Enviar al servidor...");
+  } else {
+    this.formGroup.markAllAsTouched();
+  }
+}
 
 }
