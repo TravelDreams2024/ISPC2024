70152252e5c1072c9df81b526b8b42d058d7712c
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-15 23:38:30 -0300

    nosotros css

--- FrontEnd/src/app/pages/nosotros/nosotros.component.ts
+++ FrontEnd/src/app/pages/nosotros/nosotros.component.ts
@@ -14,67 +14,68 @@ export class NosotrosComponent {
   mision = 'Desde hace 20 años, Travel Dreams ha sido la brújula que guía a aventureros de todas las edades en sus viajes más esperados. Nuestra misión trasciende un simple itinerario; es una promesa de aventura, descubrimiento y recuerdos que perduran toda la vida.';
   equipo = [
     {
+      nombre: 'Cantarino, Emmanuel',
+      rol: 'Full Stack',
+      imagen: 'assets/img/A02_avatar_hombre.png',
+      github: '#',
+      linkedin: '#',
+      perfil: '/nosotros/1'
+    },
+    {
       nombre: 'Cascallares, Facundo',
       rol: 'Full Stack',
       imagen: 'assets/img/A02_avatar_hombre.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/1'
+      perfil: '/nosotros/2'
     },
     {
       nombre: 'Fernandez, Claudio Ranses',
       rol: 'Full Stack',
       imagen: 'assets/img/A02_avatar_hombre.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/2'
+      perfil: '/nosotros/3'
     },
     {
       nombre: 'González, Natalia',
       rol: 'Full Stack',
       imagen: 'assets/img/A01_avatar_mujer.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/3'
+      perfil: '/nosotros/4'
     },
     {
       nombre: 'Navarrete, Romina Gabriela',
       rol: 'Full Stack',
       imagen: 'assets/img/A01_avatar_mujer.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/4'
+      perfil: '/nosotros/5'
     },
     {
       nombre: 'Oro, Elias',
       rol: 'Full Stack',
       imagen: 'assets/img/A02_avatar_hombre.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/1'
+      perfil: '/nosotros/6'
     },
     {
       nombre: 'Ortiz, Fernando Ariel',
       rol: 'Full Stack',
       imagen: 'assets/img/A02_avatar_hombre.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/1'
+      perfil: '/nosotros/7'
     },
     {
       nombre: 'Soto Valenzuela, Juan Gabriel',
       rol: 'Full Stack',
       imagen: 'assets/img/A02_avatar_hombre.png',
-      twitter: '#',
-      facebook: '#',
+      github: '#',
       linkedin: '#',
-      perfil: '/nosotros/1'
+      perfil: '/nosotros/8'
     },
   ];
 }
