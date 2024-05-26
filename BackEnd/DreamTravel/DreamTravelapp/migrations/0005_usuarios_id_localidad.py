82da640e3f34f15a74bc1d301afbe18e9df64ad1
Facundo Cascallares <80273681+FacundoCascallares@users.noreply.github.com>
2024-05-26 20:12:41 -0300

    Update 0005_usuarios_id_localidad.py

--- BackEnd/DreamTravel/DreamTravelapp/migrations/0005_usuarios_id_localidad.py
+++ BackEnd/DreamTravel/DreamTravelapp/migrations/0005_usuarios_id_localidad.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='usuarios',
             name='id_localidad',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='DreamTravelapp.localidad'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='DreamTravelapp.localidad'),
         ),
     ]
