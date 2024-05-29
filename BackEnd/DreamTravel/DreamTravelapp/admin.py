2c60418e9daf07bf3d8e0f702faff1330ebac0cc
vstecnic <vstecnic@gmail.com>
2024-05-28 21:01:02 -0300

    edito modelos en models.py y configuro admin.py para los modelos agregados

--- BackEnd/DreamTravel/DreamTravelapp/admin.py
+++ BackEnd/DreamTravel/DreamTravelapp/admin.py
@@ -4,38 +4,25 @@ from django.contrib import admin
 from .models import Usuarios
 from .models import Destinos
 from .models import Categorias
-from .models import Aerolinea
 from .models import MetodoPago
-from DreamTravelapp .models import Localidad
-from .models import ObraSocial
+
 
 #Registro los modelos
 class UsuariosAdmin(admin.ModelAdmin):
     list_display = ( 'nombre_usuario', 'apellido_usuario', 'direccion', 'dni', 'telefono', 'mail',)
     
 class DestinosAdmin(admin.ModelAdmin):
-    list_display = ('nombreDestino', 'precioDestino', 'fecha_salida', 'cantidadDisponible',)
-
-class AerolineaAdmin(admin.ModelAdmin):
-    list_display = ('nombreAerolinea',)
+    list_display = ('id_destino', 'nombre_Destino', 'descripcion','precio_Destino', 'fecha_salida', 'cantidad_Disponible', 'image',)
 
 class CategoriasAdmin(admin.ModelAdmin):
-    list_display = ('nombreCategoria',)
+    list_display = ('id_categoria','nombreCategoria',)
 
-class LocalidadAdmin(admin.ModelAdmin):
-    list_display = ('nombreLocalidad',)
 
 class MetodoPagoAdmin(admin.ModelAdmin):
-    list_display = ('nombrePago',)
+    list_display = ('id_metodoPago','nombrePago',)
     
-class ObraSocialAdmin(admin.ModelAdmin):
-    list_display = ('nombreObraSocial',)
-
 
 admin.site.register(Usuarios, UsuariosAdmin)
 admin.site.register(Destinos, DestinosAdmin)
 admin.site.register(Categorias, CategoriasAdmin)
-admin.site.register(Aerolinea, AerolineaAdmin)
 admin.site.register(MetodoPago, MetodoPagoAdmin)
-admin.site.register(Localidad, LocalidadAdmin)
-admin.site.register(ObraSocial, ObraSocialAdmin)
--- BackEnd/DreamTravel/DreamTravelapp/models.py
+++ BackEnd/DreamTravel/DreamTravelapp/models.py
@@ -3,20 +3,7 @@ from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.core.exceptions import ValidationError
     
-
-#Clase de la obra social del usuario
-class ObraSocial(models.Model):
-    id_obraSocial = models.AutoField(primary_key=True)
-    nombreObraSocial = models.CharField(max_length=100)
-    class Meta:
-        db_table = 'obra_social'
-        verbose_name = 'Obras Sociale'
-        verbose_name_plural = 'Obras Sociales'
-    def __str__(self):
-        return self.nombreObraSocial
-    def __unicode__(self):
-        return self.nombreObraSocial
-    
+  
    
 #Metodo de pago del usuario 
 class MetodoPago(models.Model):
@@ -43,33 +30,8 @@ class Categorias(models.Model):
         return self.nombreCategoria
     def __unicode__(self):
         return self.nombreCategoria
-    
-#Nombre de la aerolinea
-class Aerolinea(models.Model):
-    id_aerolinea = models.AutoField(primary_key=True)
-    nombreAerolinea = models.CharField(max_length=100)
-    class Meta:
-        db_table = 'aerolinea'
-        verbose_name = 'Aerolinea'
-        verbose_name_plural = 'Aerolineas'
-    def __str__(self):
-        return self.nombreAerolinea
-    def __unicode__(self):
-        return self.nombreAerolinea
-    
-#Localidad del usuario
-class Localidad(models.Model):
-    id_localidad = models.AutoField(primary_key=True)
-    nombreLocalidad = models.CharField(max_length=150)
-    class Meta:
-        db_table = 'localidad'
-        verbose_name = 'Localidade'
-        verbose_name_plural = 'Localidades'
-    def __str__(self):
-        return self.nombreLocalidad
-    def __unicode__(self):
-        return self.nombreLocalidad
- 
+       
+
 #Clase de usuario   
 class Usuarios(models.Model):
     
@@ -81,9 +43,7 @@ class Usuarios(models.Model):
     dni = models.CharField(max_length=8, validators=[MinLengthValidator(7), MaxLengthValidator(8)])
     telefono = models.CharField(max_length=50)
     mail = models.EmailField(unique=True)
-    id_localidad = models.ForeignKey(Localidad, db_column="id_localidad", on_delete=models.CASCADE)
     id_metodoPago = models.ForeignKey(MetodoPago, db_column='id_metodoPago', on_delete=models.CASCADE,)
-    id_obraSocial = models.ForeignKey(ObraSocial, db_column='id_obraSocial', on_delete=models.CASCADE)
     class Meta:
         db_table = 'usuario'
         verbose_name = 'Usuario'
@@ -105,11 +65,12 @@ def positive_viaje_validator(value):
     
 class Destinos(models.Model):
     id_destino = models.AutoField(primary_key=True)
-    nombreDestino = models.CharField(max_length=150)
-    precioDestino = models.DecimalField(max_digits=12, decimal_places=2, validators=[positive_price_validator])
+    nombre_Destino = models.CharField(max_length=150)
+    descripcion = models.TextField(default='descripcion', blank=False)
+    image = models.TextField(default='imagen', blank=False)
+    precio_Destino = models.DecimalField(max_digits=12, decimal_places=2, validators=[positive_price_validator])
     fecha_salida = models.DateTimeField(blank=False)
-    cantidadDisponible = models.IntegerField(default=12, validators=[positive_viaje_validator])
-    id_aerolinea = models.ForeignKey(Aerolinea, db_column='id_aerolinea', on_delete=models.CASCADE)
+    cantidad_Disponible = models.IntegerField(default=12, validators=[positive_viaje_validator])
     id_metodoPago = models.ForeignKey(MetodoPago, db_column='id_metodoPago', on_delete=models.CASCADE)
     id_categoria = models.ForeignKey(Categorias, db_column='id_categoria', on_delete=models.CASCADE)
     class Meta:
@@ -117,7 +78,6 @@ class Destinos(models.Model):
         verbose_name = 'Destino'
         verbose_name_plural = 'Destinos'
     def __str__(self):
-        return self.nombreDestino
+        return self.nombre_Destino
     def __unicode__(self):
-        return self.nombreDestino
-    
+        return self.nombre_Destino
