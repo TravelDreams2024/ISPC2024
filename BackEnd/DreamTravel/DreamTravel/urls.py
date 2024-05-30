1264aac429ec0f0cbe02371296e1cc0386f42c55
Ariel957 <arieljrslm957@gmail.com>
2024-05-30 09:16:24 -0300

    Api User,Login,Register

--- BackEnd/DreamTravel/DreamTravel/urls.py
+++ BackEnd/DreamTravel/DreamTravel/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DreamTravelapp.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', RegisterView.as_view(), name='register'),
 ]
---
+++ BackEnd/DreamTravel/DreamTravelapp/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
