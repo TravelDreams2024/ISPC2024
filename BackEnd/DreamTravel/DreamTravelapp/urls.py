4339cdb3534d42594a8eb738c6d10038fe120cab
Facundo Cascallares <facundocascallares@hotmail.com>
2024-05-30 20:15:32 -0300

    Corrección en archivo urls.py

--- BackEnd/DreamTravel/DreamTravelapp/urls.py
+++ BackEnd/DreamTravel/DreamTravelapp/urls.py
@@ -12,6 +12,5 @@ router.register(r'destinos',views.DestinosViewSet)
 
 #URLS
 urlpatterns = [
-    #path('', include(router.urls)),
-    path('api/v1/', include(router.urls)),
+    path('', include(router.urls)),
 ]
