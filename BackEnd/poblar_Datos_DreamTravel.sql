ef85d6bde2af7938522cbbef6130204cfb6763dd
Juan Gabriel Soto Valenzuela <99268842+vstecnic@users.noreply.github.com>
2024-05-31 02:24:11 -0300

    Update poblar_Datos_DreamTravel.sql
    Actualizacion de ingesta de datos 31-05-24

--- BackEnd/poblar_Datos_DreamTravel.sql
+++ BackEnd/poblar_Datos_DreamTravel.sql
@@ -4,13 +4,27 @@ LIMIT 0, 1000
 
 -- Date: 2024-05-28 04:30
 */
-#SCRIPT PARA POBLAR LA BASE CON LOS DATOS DE TRABAJO
-#1º SELECCIONAMOS LA BASE
+
+
+#SI LA BASE DE DATOS NO ESTA CREADA LOCALMENTE UTILIZAMOS LA SIGUIENTE SENTENCIA
+create DATABASE dreamtravel; 
+
+
+
+#SCRIPT PARA POBLAR LA BASE CON LOS DATOS DE TRABAJO 
+#SELECCIONAMOS LA BASE
 use dreamtravel;
 
+
+#1º INSERTAMOS DATOS A ROLES
+INSERT INTO rol(`id_rol`,`nombre_rol`)VALUES (NULL, 'Administrador');
+INSERT INTO rol(`id_rol`,`nombre_rol`)VALUES (NULL, 'Usuario');
+INSERT INTO rol(`id_rol`,`nombre_rol`)VALUES (NULL, 'Full Stack');
+
+
 #2º INSERTAMOS LOS DATOS DE LAS CATEGORIAS
-INSERT INTO categorias(`id_categoria`,`nombreCategoria`)VALUES (NULL, 'Nacional');
-INSERT INTO categorias(`id_categoria`,`nombreCategoria`)VALUES (NULL, 'Internacional');
+INSERT INTO dreamtravelapp_categorias(`id_categoria`,`nombreCategoria`)VALUES (NULL, 'Nacional');
+INSERT INTO dreamtravelapp_categorias(`id_categoria`,`nombreCategoria`)VALUES (NULL, 'Internacional');
 
 #3º INSERTAMOS LOS DATOS DE LOS METODOS DE PAGO
 INSERT INTO metodo_pago(`id_metodoPago`,`nombrePago`)VALUES (NULL, 'Tarjeta');
@@ -26,28 +40,20 @@ INSERT INTO destinos (`id_destino`,`nombre_Destino`,`precio_Destino`,`fecha_sali
 INSERT INTO destinos (`id_destino`,`nombre_Destino`,`precio_Destino`,`fecha_salida`,`cantidad_Disponible`,`id_categoria`,`id_metodoPago`,`descripcion`,`image`) VALUES (NULL,'Buzios',850,'2024-08-02 18:00:00.000000',15,2,1,'Playas paradisíacas, aguas cristalinas, encanto bohemio, gastronomía exquisita, actividades acuáticas, arte y cultura.','https://imgur.com/kcJGrj2');
 INSERT INTO destinos (`id_destino`,`nombre_Destino`,`precio_Destino`,`fecha_salida`,`cantidad_Disponible`,`id_categoria`,`id_metodoPago`,`descripcion`,`image`) VALUES (NULL,'Camboriú',950,'2024-08-02 18:00:00.000000',15,2,1,'Playas extensas, moderna infraestructura turística, vida nocturna animada, gastronomía diversa, relax.','https://imgur.com/JQhfiRZ');
 
-#5º ACTUALIZACION DE DESTINOS (IMAGENES AGREGADAS) SOLO SI EL PASO 4 SE CREÓ SIN LINK DE IMAGENES,CASO CONTRARIO PASAR AL PASO 6
-UPDATE destinos SET image = 'https://imgur.com/lkLUUVP' WHERE id_destino = 1;
-UPDATE destinos SET image = 'https://imgur.com/eueFEH0' WHERE id_destino = 2;
-UPDATE destinos SET image = 'https://imgur.com/fmOTpCr' WHERE id_destino = 3;
-UPDATE destinos SET image = 'https://imgur.com/sHquW2b' WHERE id_destino = 4;
-UPDATE destinos SET image = 'https://imgur.com/ziN8JzS' WHERE id_destino = 5;
-UPDATE destinos SET image = 'https://imgur.com/TcZyLCP' WHERE id_destino = 6;
-UPDATE destinos SET image = 'https://imgur.com/kcJGrj2' WHERE id_destino = 7;
-UPDATE destinos SET image = 'https://imgur.com/JQhfiRZ' WHERE id_destino = 8;
 
-#6º INSERTAMOS NOSOTROS
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Cantarino, Emmanuel', 'Full Stack', 'https://github.com/MaxiCantarino','https://www.linkedin.com/','https://imgur.com/hP7sdsS');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Cascallares, Facundo', 'Full Stack', 'https://github.com/FacundoCascallares','https://www.linkedin.com/','https://imgur.com/hP7sdsS');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Fernandez, Claudio Ranses', 'Full Stack', 'https://github.com/ccrrff2014','https://www.linkedin.com/','https://imgur.com/hP7sdsS');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'González, Natalia', 'Full Stack', 'https://github.com/Natalia24v','https://www.linkedin.com/','https://imgur.com/Qxcj00Z');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Navarrete, Romina Gabriela', 'Full Stack', 'https://github.com/rominarg','https://www.linkedin.com/','https://imgur.com/Qxcj00Z');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Oro, Elias', 'Full Stack', 'https://github.com/EliasOro2030','https://www.linkedin.com/','https://imgur.com/hP7sdsS');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Ortiz, Fernando Ariel', 'Full Stack', 'https://github.com/ariel957','https://www.linkedin.com/','https://imgur.com/hP7sdsS');
-INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`rol`,`github`,`linkedin`,`imagen`) VALUES (NULL,'Soto Valenzuela, Juan Gabriel', 'Full Stack', 'https://github.com/vstecnic','https://www.linkedin.com/','https://imgur.com/hP7sdsS');
+#5º INSERTAMOS NOSOTROS
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Cantarino, Emmanuel', 'https://github.com/MaxiCantarino','https://www.linkedin.com/','https://imgur.com/hP7sdsS', 3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Cascallares, Facundo', 'https://github.com/FacundoCascallares','https://www.linkedin.com/','https://imgur.com/hP7sdsS',3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Fernandez, Claudio Ranses', 'https://github.com/ccrrff2014','https://www.linkedin.com/','https://imgur.com/hP7sdsS',3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'González, Natalia', 'https://github.com/Natalia24v','https://www.linkedin.com/','https://imgur.com/Qxcj00Z',3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Navarrete, Romina Gabriela', 'https://github.com/rominarg','https://www.linkedin.com/','https://imgur.com/Qxcj00Z',3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Oro, Elias', 'https://github.com/EliasOro2030','https://www.linkedin.com/','https://imgur.com/hP7sdsS',3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Ortiz, Fernando Ariel', 'https://github.com/ariel957','https://www.linkedin.com/','https://imgur.com/hP7sdsS',3);
+INSERT INTO nosotros (`id_nosotros`,`nombre_apellido`,`github`,`linkedin`,`imagen`,`id_rol`) VALUES (NULL,'Soto Valenzuela, Juan Gabriel', 'https://github.com/vstecnic','https://www.linkedin.com/','https://imgur.com/hP7sdsS',3);
 
 #7º INSERTAMOS USUARIOS DE PRUEBA
-INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`, `rol`, `direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`) VALUES (NULL, 'Homero','Simpson', 'ctrlSupr', 'usuario', 'Av.Siempre Viva 742', '2745136', '0303456', 'homerjsimpson@aol.com',1);
-INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`, `rol`, `direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`) VALUES (NULL, 'Diana','Prince', 'ww2009_dc', 'administrador', '4000 Warner Boulevard, Burbank, California, Estados Unidos', '17242560', '0800101010231', 'justice_leage@yahoo.com',2);
-INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`, `rol`, `direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`) VALUES (NULL, 'Rick','Sánchez', 'SoyPickleRick','administrador', 'Calle Falsa 123', '888888', '0600242526', 'ricks@altavista.com',1);
-INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`, `rol`, `direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`) VALUES (NULL, 'Bruno','Diaz', 'ImBatman', 'usuario', 'Algun lugar de Gotham City 482 ', '227596', '4865145780', 'bruno_diaz@gotham.com',2);
+INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`,`direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`, `id_rol`) VALUES (NULL, 'Homero','Simpson', 'ctrlSupr','Av.Siempre Viva 742', '2745136', '0303456', 'homerjsimpson@aol.com',1,2);
+INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`,`direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`, `id_rol`) VALUES (NULL, 'Diana','Prince', 'ww2009_dc','4000 Warner Boulevard, Burbank, California, Estados Unidos', '17242560', '0800101010231', 'justice_leage@yahoo.com',2,1);
+INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`,`direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`, `id_rol`) VALUES (NULL, 'Rick','Sánchez', 'SoyPickleRick','Calle Falsa 123', '888888', '0600242526', 'ricks@altavista.com',2,1);
+INSERT INTO Usuario (`id_usuario`, `nombre_usuario`,`apellido_usuario`, `passw`,`direccion`, `dni`, `telefono`, `mail`, `id_metodoPago`, `id_rol`) VALUES (NULL, 'Bruno','Diaz', 'ImBatman','Algun lugar de Gotham City 482 ', '227596', '4865145780', 'bruno_diaz@gotham.com',1,2);
+
