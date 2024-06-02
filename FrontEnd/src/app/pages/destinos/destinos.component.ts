d4a02a79f6ce74317ee5b1584260701a8a2f23cd
EliasOro2030 <greendaylife@hotmail.com>
2024-06-02 01:53:16 -0300

    agrego servicios destinos

--- FrontEnd/src/app/pages/destinos/destinos.component.ts
+++ FrontEnd/src/app/pages/destinos/destinos.component.ts
@@ -1,55 +1,25 @@
-import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DestinosService } from '../../services/destinos.service';
 
 @Component({
   selector: 'app-destinos',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NgFor,NgIf, NgSwitch, NgSwitchCase],
+  imports: [],
   templateUrl: './destinos.component.html',
   styleUrl: './destinos.component.css'
 })
 export class DestinosComponent {
-  imagenList: { url: string, name: string, description: string }[] = [
-    { 
-      url: "./assets/img/01_bariloche.png", 
-      name: "San Carlos de Bariloche",
-      description: "Paisajes majestuosos, lagos cristalinos, montañas imponentes, chocolate artesanal, esquí de clase mundial y encanto alpino."
-    },
-    { 
-      url: "./assets/img/02_villa_carlos_paz.png", 
-      name: "Villa Carlos Paz",
-      description: "Lago, teatro, naturaleza, diversión, espectáculos, sierras, tranquilidad, gastronomía, balnearios, entretenimiento y relax."
-    },
-    { 
-      url: "./assets/img/03_necochea.png", 
-      name: "Necochea",
-      description: "Playas extensas, paseos costeros, faro histórico, pesca deportiva, tranquilidad, dunas imponentes, atardeceres espectaculares."
-    },
-    { 
-      url: "./assets/img/04_monte_hermoso.png", 
-      name: "Monte Hermoso",
-      description: "Playas de arenas doradas, bosques de pinos, reserva natural, actividades acuáticas, descanso, sol, familia, paz, naturaleza."
-    },
-    { 
-      url: "./assets/img/05_pehuenco.png", 
-      name: "Pehuenco",
-      description: "Majestuosa masa de hielo, imponente belleza natural, aventura glaciar, turquesa resplandeciente, paisaje patagónico único."
-    },
-    { 
-      url: "./assets/img/06_glaciar_perito_moreno.png", 
-      name: "Glaciar Perito Moreno",
-      description: "Majestuosa masa de hielo, imponente belleza natural, aventura glaciar, turquesa resplandeciente, paisaje patagónico único."
-    },
-    { 
-      url: "./assets/img/07_buzios.png", 
-      name: "Búzios",
-      description: "Playas paradisíacas, aguas cristalinas, encanto bohemio, gastronomía exquisita, actividades acuáticas, arte y cultura."
-    },
-    { 
-      url: "./assets/img/08_camboriu.png", 
-      name: "Camboriú",
-      description: "Playas extensas, moderna infraestructura turística, vida nocturna animada, gastronomía diversa, relax."
-    }
-  ];
+  destinosList:any;
+  constructor(private DestinoService:DestinosService)
+  {
+    this.destinosList=DestinoService.obtenerDestino().subscribe({
+      next: (destinosList) => {
+        this.destinosList=destinosList;
+      },
+      error: (error) => {
+        console.error(error)
+      }
+    });
+  }
+
 }
